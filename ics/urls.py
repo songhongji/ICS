a2ac44d7a159b61db20df256c9d54aa495e487f4
tutengfei <tutengfei.kevin@gmail.com>
2014-10-16 11:40:48 +0800

    fix home

--- ics/urls.py
+++ ics/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
-#admin.autodiscover()
+admin.autodiscover()
 
 import xadmin
 xadmin.autodiscover()
@@ -9,6 +8,7 @@ xadmin.autodiscover()
 from xadmin.plugins import xversion
 xversion.register_models()
 
+from user_manage import views
 
 urlpatterns = patterns('',
     # Examples:
@@ -16,5 +16,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     #url(r'^admin/', include(admin.site.urls)),
-     url(r'^xadmin/', include(xadmin.site.urls))
+    url(r'^$', views.index, name="home"),
+    url(r'^xadmin/', include(xadmin.site.urls)),
 )
